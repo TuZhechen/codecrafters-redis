b35e049283c53cbadcf48a556a9a97feb6f6bd37
Uruyuki Mori <tfb1995@gmail.com>
2024-11-23 01:26:57 +0900

    pass 4th stage -- 2nd try

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,9 +19,11 @@ public class Main {
         // ensures that we don't run into 'Address already in use' errors
         serverSocket.setReuseAddress(true);
         // Wait for connection from client.
-        clientSocket = serverSocket.accept();
-        final Socket client = clientSocket;
-        threadPool.submit(() -> handleClient(client));
+        while(true){
+            clientSocket = serverSocket.accept();
+            final Socket client = clientSocket;
+            threadPool.submit(() -> handleClient(client));
+        }
     } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
     } finally {
