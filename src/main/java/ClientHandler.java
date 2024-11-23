fe6d1dc2d0e684b9eea190974f01ae2765f41aa8
Uruyuki Mori <tfb1995@gmail.com>
2024-11-23 12:08:12 +0900

    pass 6th stage -- 1st try

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,8 +1,10 @@
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
 
 public class ClientHandler implements Runnable {
     private final Socket clientSocket;
+    private static final HashMap<String, String> store = new HashMap<>();
 
     public ClientHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -24,6 +26,29 @@ public class ClientHandler implements Runnable {
                     outputStream.write(
                             String.format("$%d\r\n%s\r\n", echo.length(), echo).getBytes()
                     );
+                } else if("SET".equalsIgnoreCase(request)) {
+                    reader.readLine();
+                    String key = reader.readLine();
+                    reader.readLine();
+                    String value = reader.readLine();
+                    synchronized (store) {
+                        store.put(key, value);
+                    }
+                    outputStream.write("+OK\r\n".getBytes());
+                } else if ("GET".equalsIgnoreCase(request)) {
+                    reader.readLine();
+                    String key = reader.readLine();
+                    String value;
+                    synchronized (store) {
+                        value = store.get(key);
+                    }
+                    if (value != null) {
+                        outputStream.write(
+                                String.format("$%d\r\n%s\r\n", value.length(), value).getBytes()
+                        );
+                    } else {
+                        outputStream.write("$-1\r\n".getBytes());
+                    }
                 }
             }
         } catch (IOException e) {
