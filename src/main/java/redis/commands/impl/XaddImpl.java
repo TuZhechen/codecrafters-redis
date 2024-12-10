11c0a5945d09dfbfbcc1aa45e47267c63c39a839
Uruyuki Mori <tfb1995@gmail.com>
2024-12-10 21:23:54 +0900

    add entry_id check logic to XADD

--- src/main/java/redis/commands/impl/XaddImpl.java
+++ src/main/java/redis/commands/impl/XaddImpl.java
@@ -26,20 +26,74 @@ public class XaddImpl implements RedisCommandHandler {
             clientHandler.getWriter().flush();
             return;
         }
+
         String key = args[1], id = args[2];
-        if(!storageManager.containsKey(key)) {
-            storageManager.put(key, new MortalValue<> (new RedisStream()));
+        if (!isValidStreamId(id)) {
+            response = "-ERR make sure stream ID: <milliSecondTimes>-<sequenceNumber>\r\n";
+            clientHandler.getWriter().print(response);
+            clientHandler.getWriter().flush();
+            return;
+        }
+
+        if (compareStreamIds(id, "0-0") <= 0) {
+            response = "-ERR The ID specified in XADD must be greater than 0-0\r\n";
+            clientHandler.getWriter().print(response);
+            clientHandler.getWriter().flush();
+            return;
+        }
+
+        MortalValue<RedisStream> streamValue = storageManager.get(key, RedisStream.class);
+        RedisStream stream;
+
+        if (streamValue == null) {
+            stream = new RedisStream();
+            storageManager.put(key, new MortalValue<>(stream));
+        } else {
+            stream = streamValue.getValue();
+        }
+
+        if (!stream.getEntries().isEmpty()) {
+            String lastId = stream.getEntries()
+                                  .get(stream.getEntries().size() - 1)
+                                  .getId();
+            if (compareStreamIds(id, lastId) <= 0) {
+                response = "-ERR The ID specified in XADD is equal or smaller than the target stream top item\r\n";
+                clientHandler.getWriter().print(response);
+                clientHandler.getWriter().flush();
+                return;
+            }
         }
 
         Map<String, String> entry = new HashMap<>();
         for (int i = 3; i < args.length; i+=2) {
             entry.put(args[i], args[i+1]);
         }
-        storageManager.get(key, RedisStream.class)
-                .getValue().addEntry(id, entry);
+        stream.addEntry(id, entry);
 
         response = RESPEncoder.encodeBulkString(id);
         clientHandler.getWriter().print(response);
         clientHandler.getWriter().flush();
+    }
+
+    private boolean isValidStreamId(String id) {
+        String[] idParts = id.split("-");
+        if (idParts.length != 2) return false;
+        try {
+            Long.parseLong(idParts[0]);
+            Long.parseLong(idParts[1]);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private int compareStreamIds(String id1, String id2) {
+        String[] parts1 = id1.split("-"), parts2 = id2.split("-");
+        long timestamp1 = Long.parseLong(parts1[0]), timestamp2 = Long.parseLong(parts2[0]);
+        if (timestamp1 != timestamp2) {
+            return Long.compare(timestamp1, timestamp2);
+        }
+        long sequence1 = Long.parseLong(parts1[1]), sequence2 = Long.parseLong(parts2[1]);
+        return Long.compare(sequence1, sequence2);
     }
 }
